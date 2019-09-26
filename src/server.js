d6f9525c13049e1e2772e1b22f42696c24ed8a5a
Michaël Zasso <targos@protonmail.com>
2019-09-26 17:30:28 +0200

    add swagger documentation

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
 'use strict';
 
-const fastify = require('fastify');
-
+const server = require('./fastify');
 const env = require('./env');
 
-const server = fastify({
-  logger: {
-    prettyPrint: env.isDevelopment,
+server.register(require('fastify-swagger'), {
+  exposeRoute: true,
+  routePrefix: '/documentation',
+  swagger: {
+    info: {
+      title: 'Label print server',
+      description: 'Print labels using an HTTP API',
+    },
+    host: 'localhost:3000',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
   },
-  trustProxy: true,
-});
-
-server.register(require('fastify-mongodb'), {
-  url: env.MONGODB_URL,
-  database: env.MONGODB_DATABASE,
-  autoReconnect: true,
 });
 
 require('./api')(server);
